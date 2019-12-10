c63d8b101631b3f36f7961f4099f43e370eab6d3
leviminch04 <54951562+leviminch04@users.noreply.github.com>
2019-12-10 09:15:40 -0800

    efyiguh

--- src/PigDice/PigDice.java
+++ src/PigDice/PigDice.java
@@ -5,15 +5,9 @@ public class PigDice
     public static void main(String[] args)
     {
         Dice dice = new Dice(6);
-        dice.roll();
-        System.out.println(dice);
-        dice.roll();
-        System.out.println(dice);
-        Score score = new Score();
-        score.scoreAdd(100);
-        System.out.println(score.getScore());
-        score.scoreMinus(1);
-        System.out.println(score.getScore());
+        Score p1Score = new Score();
+        Score p2Score = new Score();
+
 
     }
 }
