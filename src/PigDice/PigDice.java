af1b3d21ca79fa1ed714d6536bbad0bd50d02ca1
HashbrownBurger <54951157+HashbrownBurger@users.noreply.github.com>
2019-12-10 09:19:15 -0800

    o;uSIRF

--- src/PigDice/PigDice.java
+++ src/PigDice/PigDice.java
@@ -4,7 +4,8 @@ public class PigDice
 {
     public static void main(String[] args)
     {
-        Dice dice = new Dice(6);
+        Dice dice1 = new Dice(6);
+        Dice dice2 = new Dice(6)
         Score p1Score = new Score();
         Score p2Score = new Score();
 
---
+++ src/PigDice/roundScore.java
@@ -0,0 +1,4 @@
+package PigDice;
+
+public class roundScore {
+}
