50042ffd51e6227ff208616d2e4c35ef24fcc2d9
HashbrownBurger <54951157+HashbrownBurger@users.noreply.github.com>
2019-12-12 09:28:46 -0800

    Merge branch 'master' of C:\Users\Admin\Documents\GitHub\Unit-4-Final with conflicts.

--- src/PigDice/PigDiceGui.java
+++ src/PigDice/PigDiceGui.java
@@ -3,6 +3,7 @@ package PigDice;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -14,6 +15,7 @@ public class PigDiceGui extends Application
     Dice d1 = new Dice(6);
     Dice d2 = new Dice(6);
     private GridPane gridpane = new GridPane();
+    private TextField diceAmount = new TextField();
     public Turn turn = new Turn();
     public String roundString = "" + turn.getTurnNumber();
     public String playerString = "";
@@ -37,6 +39,7 @@ public class PigDiceGui extends Application
         gridpane.add(newTurn, 0, 1);
         gridpane.add(player, 0, 2);
         gridpane.add(rollDice, 100, 0);
+        gridpane.add(diceAmount, 500, 500);
         Scene scene = new Scene(gridpane, 1920, 900);
         stage.setTitle("Pig Dice");
         stage.setScene(scene);
@@ -54,66 +57,8 @@ public class PigDiceGui extends Application
     }
     private void newDiceRoll(ActionEvent actionEvent)
     {
-        if(turn.getPlayer() == 1)
-        {
-            d1.roll();
-            d2.roll();
-            if(d1.getValue() == 1)
-            {
-                if(d2.getValue() == 1)
-                {
-                    p1.setRoundScore(0);
-                    p1.setScore(0);
-                    turn.newTurn();
-                }
-                else
-                {
-                    p1.setRoundScore(0);
-                    turn.newTurn();
-                }
-            }
-            else if(d2.getValue() == 1)
-            {
-                p1.setRoundScore(0);
-                turn.newTurn();
-            }
-            else
-            {
-                p1.addRoundScore(d1.value + d2.value);
-            }
-        }
-
-        if(turn.getPlayer() == 2)
-        {
-            d1.roll();
-            d2.roll();
-            if(d1.getValue() == 1)
-            {
-                if(d2.getValue() == 1)
-                {
-                    p2.setRoundScore(0);
-                    p2.setScore(0);
-                    turn.newTurn();
-                }
-                else
-                {
-                    p2.setRoundScore(0);
-                    turn.newTurn();
-                }
-            }
-            else if(d2.getValue() == 1)
-            {
-                p2.setRoundScore(0);
-                turn.newTurn();
-            }
-            else
-            {
-                p2.addRoundScore(d1.value + d2.value);
-            }
-        }
-
-
-
-
+        d1.roll();
+        d2.roll();
+        diceAmount.setText("Value of Dice" + d1+ "     " + d2);
     }
 }
