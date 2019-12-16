769c8d60a520c9201609d44f2a5cfdc79183b47a
HashbrownBurger <54951157+HashbrownBurger@users.noreply.github.com>
2019-12-16 09:20:42 -0800

    i7v6 r

--- src/PigDice/PigDiceGui.java
+++ src/PigDice/PigDiceGui.java
@@ -36,6 +36,7 @@ public class PigDiceGui extends Application
     Button newTurn = new Button("Start Game");
     Button rollDice = new Button("Roll");
     Font font = new Font(12);
+    newTurnPress turn = new newTurnPress();
 
 
     public static void main(String[] args) {
@@ -66,28 +67,30 @@ public class PigDiceGui extends Application
 
     private void newTurnPress(ActionEvent actionEvent)
     {
-        if(chnageTurnName)
-        {
-            newTurn.setText("Pass");
-        }
+//        if(chnageTurnName)
+//        {
+//            newTurn.setText("Pass");
+//        }
+//
+//        turn.newTurn();
+//        roundString = "round: " + turn.getTurnNumber();
+//        round.setText(roundString);
+//        playerString = "player " + turn.getPlayer() + " turn";
+//        player.setText(playerString);
+//        p1.scoreAdd(p1.getRoundScore());
+//        p1TextString = "Player 1's score: "  + p1.getScore();
+//        p1Text.setText(p1TextString);
+//        p1.setRoundScore(0);
+//        p1RoundScoreString = "Player 1's round score: " + p1.getRoundScore();
+//        p1RoundScore.setText(p1RoundScoreString);
+//        p2.scoreAdd(p2.getRoundScore());
+//        p2TextString = "Player 1's score: "  + p2.getScore();
+//        p2Text.setText(p2TextString);
+//        p2.setRoundScore(0);
+//        p2RoundScoreString = "Player 2's round score: " + p2.getRoundScore();
+//        p2RoundScore.setText(p2RoundScoreString);
+        turn.turnPress();
 
-        turn.newTurn();
-        roundString = "round: " + turn.getTurnNumber();
-        round.setText(roundString);
-        playerString = "player " + turn.getPlayer() + " turn";
-        player.setText(playerString);
-        p1.scoreAdd(p1.getRoundScore());
-        p1TextString = "Player 1's score: "  + p1.getScore();
-        p1Text.setText(p1TextString);
-        p1.setRoundScore(0);
-        p1RoundScoreString = "Player 1's round score: " + p1.getRoundScore();
-        p1RoundScore.setText(p1RoundScoreString);
-        p2.scoreAdd(p2.getRoundScore());
-        p2TextString = "Player 1's score: "  + p2.getScore();
-        p2Text.setText(p2TextString);
-        p2.setRoundScore(0);
-        p2RoundScoreString = "Player 2's round score: " + p2.getRoundScore();
-        p2RoundScore.setText(p2RoundScoreString);
 
 
     }
