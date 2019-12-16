a572a43bb3536656792f73288c6af47b79f2caf2
leviminch04 <54951562+leviminch04@users.noreply.github.com>
2019-12-16 09:24:03 -0800

    efyiguh

--- src/PigDice/Turn.java
+++ src/PigDice/Turn.java
@@ -9,9 +9,11 @@ public class Turn {
     }
     public void newTurn() {
         turnNumber++;
+        //even numbers = player 2
         if (turnNumber % 2 == 0) {
             player = 2;
         }
+        //odd numbers = player 1
         else
             player = 1;
     }
